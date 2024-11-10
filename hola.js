20c8b0571e4c9d216dacc083828c556e6867a60e
GabrielaTumbaco <gltumbaco@uce.edu.ec>
2024-11-10 16:12:31 -0500

    Program in Javascript language that displays a hello this is my first Java Script

---
+++ hola.js
@@ -0,0 +1,9 @@
+// Función principal que imprime un mensaje en la consola
+function imprimirMensaje() {
+    const mensaje = "¡Hola, mundo! Este es un mensaje en JavaScript estructurado.";
+    console.log(mensaje);
+  }
+  
+  // Llamada a la función principal
+  imprimirMensaje();
+  
